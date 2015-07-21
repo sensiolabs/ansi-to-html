4b22536adf0b40f4236fa906581a264146790ba7
André Püschel <andre.pueschel@bcs-sachsen.de>
2015-07-21 09:58:06 +0200

    adding php version checks to keep compatibility with php 5.3

--- SensioLabs/AnsiConverter/AnsiToHtmlConverter.php
+++ SensioLabs/AnsiConverter/AnsiToHtmlConverter.php
@@ -41,7 +41,7 @@ class AnsiToHtmlConverter
     {
         // remove cursor movement sequences
         $text = preg_replace('#\e\[(K|s|u|2J|2K|\d+(A|B|C|D|E|F|G|J|K|S|T)|\d+;\d+(H|f))#', '', $text);
-        $text = htmlspecialchars($text, ENT_QUOTES | ENT_SUBSTITUTE, $this->charset);
+        $text = htmlspecialchars($text, (PHP_VERSION_ID >= 50400 ? ENT_QUOTES | ENT_SUBSTITUTE : ENT_QUOTES), $this->charset);
 
         // carriage return
         $text = preg_replace('#^.*\r(?!\n)#m', '', $text);
