7ac656773a65d8b40ef7f232e9405ab10f5b0769
Jack'lul <jacklulcat@gmail.com>
2025-02-26 20:10:20 +0100

    Add theme with ANSI-like colors

---
+++ SensioLabs/AnsiConverter/Theme/AnsiTheme.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of ansi-to-html.
+ *
+ * (c) 2013 Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SensioLabs\AnsiConverter\Theme;
+
+/**
+ * This theme uses ANSI-like colors.
+ */
+class AnsiTheme extends Theme
+{
+    public function asArray()
+    {
+        return array(
+            // normal
+            'black' => '#000000',
+            'red' => '#AA0000',
+            'green' => '#00AA00',
+            'yellow' => '#AAAA00',
+            'blue' => '#0000AA',
+            'magenta' => '#AA00AA',
+            'cyan' => '#00AAAA',
+            'white' => '#AAAAAA',
+
+            // bright
+            'brblack' => '#555555',
+            'brred' => '#FF5555',
+            'brgreen' => '#55FF55',
+            'bryellow' => '#FFFF55',
+            'brblue' => '#5555FF',
+            'brmagenta' => '#FF55FF',
+            'brcyan' => '#55FFFF',
+            'brwhite' => '#FFFFFF',
+        );
+    }
+}
