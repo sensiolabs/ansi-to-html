377560018ec6ad5c7dfcc74ee509d76b358d7944
Fabien Potencier <fabien@potencier.org>
2025-03-15 10:00:55 -0700

    Fix license headers

--- SensioLabs/AnsiConverter/Bridge/Twig/AnsiExtension.php
+++ SensioLabs/AnsiConverter/Bridge/Twig/AnsiExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of ansi-to-html.
+ *
+ * (c) Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SensioLabs\AnsiConverter\Bridge\Twig;
 
 use SensioLabs\AnsiConverter\AnsiToHtmlConverter;
